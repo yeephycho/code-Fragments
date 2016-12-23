a8ae674f0342e272eb7422ff5097d87a5a74c166
yeephycho <huyixuanhyx@live.cn>
2016-12-23 09:43:18 +0800

    coding style

--- SET_CPU_AFFINITY/my_scheduler.cc
+++ SET_CPU_AFFINITY/my_scheduler.cc
@@ -10,44 +10,49 @@
 #include<ctype.h>
 #include<string.h>
 
-int main(int argc,char* argv[]){    
-    struct sched_param param;
-    long int ret = 0;    
+int
+main(int argc,char* argv[]) {    
+  struct sched_param param;
+  long int ret = 0;    
     
-    cpu_set_t mask ;
-    CPU_ZERO(&mask);
-    CPU_SET(1,&mask);
+  cpu_set_t mask ;
+  CPU_ZERO(&mask);
+  CPU_SET(1,&mask);
 
-    if (sched_setaffinity(0, sizeof(mask), &mask) == -1)
-    {
-        printf("warning: could not set CPU affinity, continuing...\n");
-    }
+  if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
+    printf("warning: could not set CPU affinity, continuing...\n");
+  }
     
-    printf("current pid is %ld\n", getpid());
-    sched_setaffinity(getpid(), sizeof(mask), &mask);
-    sched_getaffinity(getpid(), sizeof(mask), &mask);
-    printf("current cpu affinity mask is %d\n", mask);
-    
-//    CPU_ZERO(&mask);
-    CPU_CLR(1, &mask);
-    CPU_SET(2, &mask);
-    
-    printf("current pid is %ld\n", getpid());
-    sched_setaffinity(getpid(), sizeof(mask), &mask);
-    sched_getaffinity(getpid(), sizeof(mask), &mask);
-    printf("current cpu affinity mask is %d\n", mask);
-    
-    int sched_method = SCHED_FIFO;
-    int sched_priority = sched_get_priority_max(SCHED_FIFO);
+  printf("current pid is %ld\n", getpid());
+  sched_setaffinity(getpid(), sizeof(mask), &mask);
+  sched_getaffinity(getpid(), sizeof(mask), &mask);
+  printf("current cpu affinity mask is %d\n", mask);
 
-    param.sched_priority = sched_priority;
-    ret = sched_setscheduler(getpid(), sched_method, &param);
-    if(ret){
-        fprintf(stderr, "set scheduler to %ld, failed %m\n", getpid());
-        return -4;
-    }
+/*
+** CPU_ZERO(&mask);
+*/
+
+  CPU_CLR(1, &mask);
+  CPU_SET(2, &mask);
     
-    int scheduler = sched_getscheduler(getpid());
+  printf("current pid is %ld\n", getpid());
+  sched_setaffinity(getpid(), sizeof(mask), &mask);
+  sched_getaffinity(getpid(), sizeof(mask), &mask);
+  printf("current cpu affinity mask is %d\n", mask);
     
-    fprintf(stderr, "The scheduler of PID(%ld) is %d, priority (%d)\n", getpid(), scheduler, sched_priority);    
+  int sched_method = SCHED_FIFO;
+  int sched_priority = sched_get_priority_max(SCHED_FIFO);
+
+  param.sched_priority = sched_priority;
+  ret = sched_setscheduler(getpid(), sched_method, &param);
+  if (ret) {
+    fprintf(stderr, "set scheduler to %ld, failed %m\n", getpid());
+    return -4;
+  }
+    
+  int scheduler = sched_getscheduler(getpid());
+    
+  fprintf(stderr, "The scheduler of PID(%ld) is %d, priority (%d)\n", getpid(), scheduler, sched_priority);    
+  
+  return 0;
 }
--- SET_CPU_AFFINITY/set_proc_sched.cc
+++ SET_CPU_AFFINITY/set_proc_sched.cc
@@ -14,45 +14,44 @@
 #define MILLION 1000000L
 #define NANOSECOND 1000
 
-void test_func()
-{
-    int i = 0;
-    unsigned long long result = 0;;
+void
+test_func() {
+  int i = 0;
+  unsigned long long result = 0;;
 
-    for(i = 0; i<8000 ;i++)
-    {
-        result += 2;
-    }
-
+  for(i = 0; i<8000 ;i++) {
+    result += 2;
+  }
 }
-int main(int argc,char* argv[])
-{
-    int i;
-    struct timespec sleeptm;
-    long interval;
-    struct timeval tend,tstart;
-    struct tm lcltime = {0};
-    struct sched_param param;
-    int ret = 0;
 
-    if(argc != 3)
-    {
-        fprintf(stderr,"usage:./test sched_method sched_priority\n");
-        return -1;
-    }
+int
+main(int argc,char* argv[]) {
+  int i;
+  struct timespec sleeptm;
+  long interval;
+  struct timeval tend,tstart;
+  struct tm lcltime = {0};
+  struct sched_param param;
+  int ret = 0;
 
-    cpu_set_t mask ;
-    CPU_ZERO(&mask);
-    CPU_SET(1,&mask);
+  if (argc != 3) {
+    fprintf(stderr,"usage:./test sched_method sched_priority\n");
+    return -1;
+  }
 
-    if (sched_setaffinity(0, sizeof(mask), &mask) == -1)
-    {
-        printf("warning: could not set CPU affinity, continuing...\n");
-    }
-    int sched_method = atoi(argv[1]);
-    int sched_priority = atoi(argv[2]);
+  cpu_set_t mask ;
+  CPU_ZERO(&mask);
+  CPU_SET(1,&mask);
 
-    /*    if(sched_method > 2 || sched_method < 0)
+  if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
+    printf("warning: could not set CPU affinity, continuing...\n");
+  }
+  
+  int sched_method = atoi(argv[1]);
+  int sched_priority = atoi(argv[2]);
+
+#if 0
+        if(sched_method > 2 || sched_method < 0)
         {
         fprintf(stderr,"sched_method scope [0,2]\n");
         return -2;
@@ -63,36 +62,33 @@ int main(int argc,char* argv[])
         return -3;
         }
 
-        if(sched_method == 1 || sched_method == 2)*/
-    {
-        param.sched_priority = sched_priority;
-        ret = sched_setscheduler(getpid(),sched_method,&param);
-        if(ret)
-        {
-            fprintf(stderr,"set scheduler to %d %d failed %m\n");
-            return -4;
-        }
+        if(sched_method == 1 || sched_method == 2)
+#endif // #if 0
+
+  {
+    param.sched_priority = sched_priority;
+    ret = sched_setscheduler(getpid(),sched_method,&param);
+    if (ret) {
+      fprintf(stderr,"set scheduler to %d %d failed %m\n");
+      return -4;
     }
+  }
 
-    int scheduler = sched_getscheduler(getpid());
+  int scheduler = sched_getscheduler(getpid());
 
-    fprintf(stderr,"the scheduler of PID(%ld) is %d, priority (%d),BEGIN time is :%ld\n",
-            getpid(),scheduler,sched_priority,time(NULL));
+  fprintf(stderr,"the scheduler of PID(%ld) is %d, priority (%d),BEGIN time is :%ld\n", getpid(),scheduler,sched_priority,time(NULL));
 
-    sleep(2);
-    sleeptm.tv_sec = 0;
-    sleeptm.tv_nsec = NANOSECOND;
+  sleep(2);
+  sleeptm.tv_sec = 0;
+  sleeptm.tv_nsec = NANOSECOND;
 
-    for(i = 0;i<COUNT;i++)
-    {
-        test_func();
-    }
+  for(i = 0;i<COUNT;i++) {
+    test_func();
+  }
 
+  interval = MILLION*(tend.tv_sec - tstart.tv_sec) + (tend.tv_usec-tstart.tv_usec);
 
-
-    interval = MILLION*(tend.tv_sec - tstart.tv_sec)
-               +(tend.tv_usec-tstart.tv_usec);
-
-    fprintf(stderr," PID = %d\t priority: %d\tEND TIME is %ld\n",getpid(),sched_priority,time(NULL));
-    return 0;
+  fprintf(stderr," PID = %d\t priority: %d\tEND TIME is %ld\n",getpid(),sched_priority,time(NULL));
+    
+  return 0;
 }
