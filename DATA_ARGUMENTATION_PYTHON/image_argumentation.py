82bb559aeb439bc1c964f4f7fcd901ebf65ac374
yeephycho <huyixuanhyx@live.cn>
2017-01-13 17:16:50 +0800

    add os function

--- DATA_ARGUMENTATION_PYTHON/image_argumentation.py
+++ DATA_ARGUMENTATION_PYTHON/image_argumentation.py
@@ -3,6 +3,7 @@
 # This file define python script to get image properities.
 
 import cv2
+import os
 
 def crop_center_square(image):
     if(2 == image.ndim):
@@ -55,16 +56,21 @@ def crop_square(image, percentage):
         crop_image = image[0:(2*radius), start_width:(start_width + 2 * radius)]
         return crop_image
 
-
-image = cv2.imread("../test_image/lennon-1.jpg")
+image = cv2.imread("./test_image/lennon-1.jpg")
 print image.shape
 #crop_image = crop_center_square(image)
 crop_image = crop_square(image, 100)
 print crop_image.shape
-cv2.imwrite("../test_image/square.jpg", crop_image)
+
+try:
+    os.stat("./test_image/output/")
+except:
+    os.mkdir("./test_image/output/")
+
+cv2.imwrite("./test_image/output/square.jpg", crop_image)
 rsz_image = cv2.resize(crop_image, (256, 256))
 print rsz_image.shape
-cv2.imwrite("../test_image/rsz_image.jpg", rsz_image)
+cv2.imwrite("./test_image/output/rsz_image.jpg", rsz_image)
 
 rsz_image2 = cv2.resize(crop_image, (20, 20))
-cv2.imwrite("../test_image/small_image.jpg", rsz_image2)
+cv2.imwrite("./test_image/output/small_image.jpg", rsz_image2)
